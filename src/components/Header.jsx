79ab3ac0639c26a73e87a4abfcf742be142f9dde
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-07-03 22:15:22 -0400

    Keeper App 1st Commit
    First commit includes the basis from which the Keeper App will be built.
    App is constructed using React Components at this time.
    Components featured are Header, Footer, and Note.
    An additional challenge initiated by me is to begin incorporating Sass into the writing of this project. 
    At this time all base and component files for Sass are incorporated into the main.css file.
    main.css was created due to the use of VSC Sass Watch feature as React was already initiated. Will look into seeing if there are any alternatives as I would rather not have main.css.map file.

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <header><h1>Orion's Keeper App</h1><p>Made with React!</p></header>
+    );
+}
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+function Note () {
+    return (
+        <div className="note">
+            <h1> What Time It Is?</h1>
+            <p>Its React Writing Time!</p>
+        </div>
+    );
+}
+
+export default Note;
